860392f290366d20ff58a9e34e575ec4324d0347
KristerBC <kristoffer.brastrup@etu.univ-lyon1.fr>
2017-03-15 09:36:30 +0100

    Added AdminLTE

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('admin', function () {
+    return view('admin_template');
+});
+
+Route::get('test', 'IndexController@index');
