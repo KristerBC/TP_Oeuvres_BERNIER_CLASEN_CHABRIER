4c9b52191a12f0656cf6506dea7622e645ece2c5
Eriwyr <mail@v-chabrier.me>
2017-04-19 23:16:28 +0200

    patch header display

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -2,7 +2,7 @@
 <header class="main-header">
 
     <!-- Logo -->
-    <a href="index2.html" class="logo"><b>Oeuvres</b>
+    <a href="index2.html" class="logo"><b>Oeuvres</b></a>
 
     <!-- Header Navbar -->
     <nav class="navbar navbar-static-top" role="navigation">
